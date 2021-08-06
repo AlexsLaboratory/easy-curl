d066019e542593c6c81e5c4fd5ba906a0a3f01d3
Alex Lowe <loweman64@outlook.com>
2021-08-05 23:56:51 -0400

    test: Update Tests For Error Handler

--- tests/EasyCurlTest.php
+++ tests/EasyCurlTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Lowem\EasyCurl\EasyCurl;
+use Lowem\EasyCurl\HTTPRequestException;
 use PHPUnit\Framework\TestCase;
 
 class EasyCurlTest extends TestCase {
@@ -8,44 +9,66 @@ class EasyCurlTest extends TestCase {
     $test = new EasyCurl("http://192.168.160.137:8080/geoserver/rest/workspaces/World_Claim/coveragestores/test7/file.geotiff?configure=first&coverageName=test7");
     $test->setBasicAuth("admin", "geoserver");
     $file = file_get_contents('C:/Users/LoweM/Downloads/wc2.1_2.5m_prec_2010-2018/wc2.1_2.5m_prec_2010-01.tif');
-    $test->put($file, [
-      "Content-Type: image/tiff",
-    ]);
-
-    $this->assertIsNotNumeric($test->getErrorCode());
+    $error = FALSE;
+    try {
+      $test->put($file, [
+        "Content-Type: image/tiff",
+      ]);
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
+    self::assertFalse($error, "Something happened");
     $test->close();
   }
 
   public function testPost() {
     $test = new EasyCurl("http://192.168.160.137:8080/geoserver/rest/workspaces?default=false");
     $test->setBasicAuth("admin", "geoserver");
-    $test->post("<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<workspace>
-    <name>Test11</name>
-</workspace>", [
-      "Content-Type: application/xml"
-    ]);
+    $error = FALSE;
+    try {
+      $test->post("<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+  <workspace>
+      <name>Test11</name>
+  </workspace>", [
+        "Content-Type: application/xml"
+      ]);
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
+    self::assertFalse($error, "Something happened");
 
-    $this->assertIsNotNumeric($test->getErrorCode());
     $test->close();
   }
 
   public function testGet() {
     $test = new EasyCurl("http://192.168.160.137:8080/geoserver/rest/workspaces");
     $test->setBasicAuth("admin", "geoserver");
-    $test->get();
+    $error = FALSE;
+    try {
+      $test->get();
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
+    self::assertFalse($error, "Something happened");
     print_r($test->getExecMessage());
 
-    $this->assertIsNotNumeric($test->getErrorCode());
     $test->close();
   }
 
   public function testDelete() {
     $test = new EasyCurl("http://192.168.160.137:8080/geoserver/rest/workspaces/Test1?recurse=true");
     $test->setBasicAuth("admin", "geoserver");
-    $test->delete();
-
-    $this->assertIsNotNumeric($test->getErrorCode());
+    $error = FALSE;
+    try {
+      $test->delete();
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
+    self::assertFalse($error, "Something happened");
     $test->close();
   }
 }
