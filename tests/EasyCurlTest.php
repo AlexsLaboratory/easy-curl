4c187d1898907f478c59e0bf0e8a2a30ef5fd23c
Alex Lowe <loweman64@outlook.com>
2021-08-06 22:53:31 -0400

    test: Change All Tests to Run On Dummy API Endpoints

--- tests/EasyCurlTest.php
+++ tests/EasyCurlTest.php
@@ -6,45 +6,51 @@ use PHPUnit\Framework\TestCase;
 
 class EasyCurlTest extends TestCase {
   public function testPut() {
-    $test = new EasyCurl("http://192.168.160.137:8080/geoserver/rest/workspaces/World_Claim/coveragestores/test7/file.geotiff?configure=first&coverageName=test7");
-    $test->setBasicAuth("admin", "geoserver");
-    $file = file_get_contents('C:/Users/LoweM/Downloads/wc2.1_2.5m_prec_2010-2018/wc2.1_2.5m_prec_2010-01.tif');
+    $test = new EasyCurl("https://jsonplaceholder.typicode.com/posts/1");
     $error = FALSE;
     try {
-      $test->put($file, [
-        "Content-Type: image/tiff",
+      $data = [
+        "id" => 1,
+        "title" => "foo",
+        "body" => "bar",
+        "userId" => 1
+      ];
+      $test->put(json_encode($data), [
+        "Content-type: application/json; charset=UTF-8",
       ]);
     } catch (HTTPRequestException $e) {
       $error = TRUE;
       echo $e->getCustomMessage();
     }
     self::assertFalse($error, "Something happened");
+    print_r($test->getExecMessage());
     $test->close();
   }
 
   public function testPost() {
-    $test = new EasyCurl("http://192.168.160.137:8080/geoserver/rest/workspaces?default=false");
-    $test->setBasicAuth("admin", "geoserver");
+    $test = new EasyCurl("https://jsonplaceholder.typicode.com/posts");
     $error = FALSE;
     try {
-      $test->post("<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-  <workspace>
-      <name>Test11</name>
-  </workspace>", [
-        "Content-Type: application/xml"
+      $data = [
+        "title" => "foo",
+        "body" => "bar",
+        "userId" => 1
+      ];
+      $test->post(json_encode($data), [
+        "Content-type: application/json; charset=UTF-8",
       ]);
     } catch (HTTPRequestException $e) {
       $error = TRUE;
       echo $e->getCustomMessage();
     }
     self::assertFalse($error, "Something happened");
+    print_r($test->getExecMessage());
 
     $test->close();
   }
 
   public function testGet() {
-    $test = new EasyCurl("http://192.168.160.137:8080/geoserver/rest/workspaces");
-    $test->setBasicAuth("admin", "geoserver");
+    $test = new EasyCurl("https://jsonplaceholder.typicode.com/posts");
     $error = FALSE;
     try {
       $test->get();
@@ -59,8 +65,7 @@ class EasyCurlTest extends TestCase {
   }
 
   public function testDelete() {
-    $test = new EasyCurl("http://192.168.160.137:8080/geoserver/rest/workspaces/Test1?recurse=true");
-    $test->setBasicAuth("admin", "geoserver");
+    $test = new EasyCurl("https://jsonplaceholder.typicode.com/posts/1");
     $error = FALSE;
     try {
       $test->delete();
@@ -69,6 +74,8 @@ class EasyCurlTest extends TestCase {
       echo $e->getCustomMessage();
     }
     self::assertFalse($error, "Something happened");
+    print_r($test->getExecMessage());
+
     $test->close();
   }
 }
